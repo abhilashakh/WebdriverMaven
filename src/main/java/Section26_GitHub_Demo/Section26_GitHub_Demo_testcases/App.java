8f1068f6dc86360e8c1528111ab5e646644bb5f7
Abhilasha Khare <abhilashakh@abhilashasmbp2.lan>
2021-02-05 14:25:10 -0800

    This is initial commit

---
+++ src/main/java/Section26_GitHub_Demo/Section26_GitHub_Demo_testcases/App.java
@@ -0,0 +1,13 @@
+package Section26_GitHub_Demo.Section26_GitHub_Demo_testcases;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Section26_GitHub_Demo/LoginTest.java
@@ -0,0 +1,43 @@
+package Section26_GitHub_Demo;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+		
+	}
+	@Test
+	public void doLogin() {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		
+	}
+	
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
