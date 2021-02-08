4ce5b0d9452ec4da9b57fd7226879d6404e3048a
Abhilasha Khare <abhilashakh@abhilashasmbp2.lan>
2021-02-08 11:07:21 -0800

    Committing changes to the login test

--- src/test/java/Section26_GitHub_Demo/LoginTest.java
+++ src/test/java/Section26_GitHub_Demo/LoginTest.java
@@ -31,6 +31,7 @@ public class LoginTest {
 		
 		driver.get("http://gmail.com");
 		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		driver.findElement(By.linkText("Learn more")).click();
 		
 	}
 	
